5c508c64f1c45240fb32e811c17e1cb0cf57a9c1
HariramRegar <regarhariram836@gmail.com>
2020-04-12 11:54:24 +0530

    initial commit

--- myBlog/index.js
+++ myBlog/index.js
@@ -7,14 +7,29 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 const port = 3000;
+var posts = [{title:"Title",content: "Content"}];
 
 app.get("/", function(req, res) {
-  res.render('index');
+  res.render('index', {posts:posts});
 });
 
-app.post("/", function(req,res) {
-  var ans = Number(req.body.a)+ Number(req.body.b);
-  res.send("Sum of numbers is "+ ans);
+app.get("/about", function(req, res) {
+  res.render('about', {about:"About"});
 });
+
+app.get("/contact", function(req, res) {
+  res.render('contact', {contact:"Contact"});
+});
+
+app.get("/compose", function(req, res) {
+  res.render('compose');
+});
+
+app.post("/compose", function(req, res) {
+  var post = req.body;
+  posts.push(post);
+  res.redirect('/compose');
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
