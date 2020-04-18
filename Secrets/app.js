5953695675e7038dbcfe16be8d26574fbc13d8b1
HariramRegar <regarhariram836@gmail.com>
2020-04-18 09:01:31 +0530

    intial commit

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5')
+//const md5 = require('md5')
 // const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -41,17 +43,22 @@ app.get('/login', function(req, res) {
 
 app.post("/login", function(req, res) {
 
-  const  username= req.body.username;
-  const  password= md5(req.body.password);
+  const username = req.body.username;
+  // const  password= md5(req.body.password);
+  const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser) {
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
-    } else{
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render('secrets');
-        }
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result == true) {
+            res.render('secrets');
+          }
+        });
       }
     }
   });
@@ -62,21 +69,23 @@ app.get('/register', function(req, res) {
 });
 
 app.post('/register', function(req, res) {
-  const user = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  user.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('secrets');
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const user = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    user.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets');
+      }
+    });
   });
 });
 
 
-app.get('/logout', function(req, res){
+app.get('/logout', function(req, res) {
   req.logout;
   res.redirect('/');
 });
