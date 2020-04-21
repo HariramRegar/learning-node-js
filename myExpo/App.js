1b08446e0832ef52c305e756c5964aec90d30032
HariramRegar <regarhariram836@gmail.com>
2020-04-21 07:58:41 +0530

    initial commit

---
+++ myExpo/App.js
@@ -0,0 +1,28 @@
+import Expo from 'expo';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Ball from './src/ball';
+
+export default function App() {
+  return ( < View style = {
+      styles.container
+    } >
+    <
+    Ball / >
+    <
+    /View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
---
+++ myExpo/src/ball.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Animated } from 'react-native';
+
+console.disableYellowBox = true;
+class Ball extends Component {
+  componentWillMount() {
+    // console.disableYellowBox = true;
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: { x: 300, y: 580 }
+    }).start();
+  }
+
+  render() {
+    return (
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
+    );
+  }
+}
+
+const styles = {
+  ball: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    borderWidth: 30,
+    borderColor: 'black'
+  }
+};
+
+export default Ball;
